ab023bf9c00fcae5143278876eff1773cd3a7fdd
cjarabek <chris.jarabek@pureweb.com>
2020-09-02 17:18:04 -0600

    First pass of availble/presales/locked flag.

--- src/pages/Workspace/Workspace.js
+++ src/pages/Workspace/Workspace.js
@@ -15,6 +15,7 @@ const stripePromise = loadStripe('pk_live_51HHEqjHyf8bGDaR7P9FvP0WZYtp2eHoeal1L2
 
 const Workspace = (props) => {
   const [isPaymentLoading, toggleLoadPayment] = useState(false);
+  const [deploymentStatus, setDeploymentStatus] = useState('available');  
 
   const fetchUserData = async () => {
     const headers = new Headers();
@@ -28,11 +29,32 @@ const Workspace = (props) => {
     }).then(res => res.json())
     .then(data => props.setUserData(data))
     .catch(err => log.error(err))
+  }  
+  const fetchDeploymentStatus = async () => {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+    await fetch('https://qamxec6q0b.execute-api.eu-central-1.amazonaws.com/prod/deploymentstatus')
+    .then(res => res.json())
+    .then(data => { 
+        if (data.body){
+          var status=JSON.parse(data.body);          
+          if (status.status){
+            setDeploymentStatus(status.status);            
+          }
+        }
+      }
+      )
+    .catch(err => {      
+      log.error(err);
+    })
   }
 
 
   useEffect(() => {
     fetchUserData();
+    fetchDeploymentStatus();    
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -59,11 +81,10 @@ const Workspace = (props) => {
     const result = await stripe.redirectToCheckout({
       sessionId: session.sessionId
     }).then().catch(err => console.log(err))
-
+  
     console.log(result);
   }
-  let playTimeRemaining = props.user && props.user.playerData.PlayTime > 0;
-  
+  let playTimeRemaining = props.user && props.user.playerData.PlayTime > 0;    
   return (
     <section className="workspace">
       <Header />
@@ -71,16 +92,30 @@ const Workspace = (props) => {
         <div className="account">
           <h2 className="workspace-title">Hi {props.user && props.user.userAttributes.nickname}, welcome to the Infinite Playa! </h2>
           <div className="account-info">
-            <div>                        
-              You current have {props.user && `${Math.floor(props.user.playerData.PlayTime)} `} minutes of play time in the Infinite Playa.<br/>
-              You can purchase more time below.                           
-            </div>
             <div>
+              You current have {props.user && `${Math.floor(props.user.playerData.PlayTime)} `} minutes of play time in the Infinite Playa.<br/><br/>
+              {
+                deploymentStatus==='prelaunch' && (
+                  <div>We are currently in pre-launch sale of Playa game time.  To ensure the best possible experience for you while we roll out the Infinite Playa
+                  we will be limiting the number of hours available for purchase. <br/><br/>
+                  
+                  You can purchase play time in the Infinite Playa below.  <br/>
+                  Access to the Playa will be opening on Friday September 11th at 12:00 PM Pacific Time.<br/><br/>
+                  We can't wait to see you!  
+                  </div>                  
+                )
+              }{
+                deploymentStatus==='available' && (
+                  <div>                                        
+                    You can purchase more time below. <br/>
+                  </div>
+                )
+              }                          
             </div>
           </div>
         </div>
         {
-            playTimeRemaining && (
+            (deploymentStatus==='available' || deploymentStatus==='locked') && playTimeRemaining && (
                <div className="workspace-button">
                 <Link to="/streamer"><button>Start playing now</button> </Link>
                 <a href={props.user && props.user.playerData.AllowPCClient ? "https://drive.google.com/file/d/1TzWSWrMCF_q4R_0todXqMDUUM0ZIyVa3/view" : '#'}>
@@ -88,7 +123,7 @@ const Workspace = (props) => {
                 </a>
                 </div>
             )
-}{
+        }{
             !playTimeRemaining && (
               <div className="workspace-button">
                <a href="/workspace#purchase"><button>Purchase Time to begin playing</button> </a>
@@ -103,60 +138,52 @@ const Workspace = (props) => {
             <a href="https://donate.burningman.org/?utm_source=infinite-playa&utm_medium=donate&utm_campaign=multiverse" target="_blank"></a>
           </div>
         </div>
+
+        { deploymentStatus==='locked' && (
+                  <div className="locked">
+                    While we work to ensure the best possible experience for you, we have temporarily stopped the purchase of game time. <br/><br/>
+                    If you have already purchased game time in the Playa, feel free to use it.  We'll be re-launching purchases shortly.  Stay tuned.
+                  </div>
+                )
+    } { (deploymentStatus==='available' || deploymentStatus==='prelaunch' )&& (
+            
         <div className="workspace-block">
           <h2>Pay as you go:</h2>
-          <p>For the interactive gaming experience, streamed directly into your Safari or Chrome, browser purchase one of our hourly passes.*</p>
-        </div>
-        {/* <div className="workspace-prices">
-          <div>
-            <span>Visitor</span>
-            <span>Two Hour Pass - $20	</span>
-          </div>
-          <div>
-            <span>Weekend Warrior	</span>
-            <span>Five Hour Pass - $40	</span>
+          <p>For the interactive gaming experience, streamed directly into your Safari or Chrome, browser purchase one of our hourly passes.*</p> 
+          <div id="purchase" className="purchases">
+            <div className="purchase-item">
+              <h3>Visitor</h3>
+              <span className="purchase-amount">$20</span>
+              <span>Two Hour Pass</span>
+              <button onClick={() => paymentFunc('price_1HLfpUHyf8bGDaR7VcuqATLR')}>Select</button>
             </div>
-          <div>
-            <span>Dusty Explorer	</span>
-            <span>Ten Hour Pass - $75	</span>
-          </div>
-          <div>
-            <span>Founder's Package</span>
-            <span>24 Hour  Pass - $150 *includes executable</span>
+            <div className="purchase-item">
+              <h3>Weekend Warrior</h3>
+              <span className="purchase-amount">$40</span>
+              <span>Five Hour Pass</span>
+              <button onClick={() => paymentFunc('price_1HLE6iLSQ0UOTq7wVOsoU30M')}>Select</button>
             </div>
-          </div> */}
-        <div id="purchase" className="purchases">
-          <div className="purchase-item">
-            <h3>Visitor</h3>
-            <span className="purchase-amount">$20</span>
-            <span>Two Hour Pass</span>
-            <button onClick={() => paymentFunc('price_1HLfpUHyf8bGDaR7VcuqATLR')}>Select</button>
+            <div className="purchase-item">
+              <h3>Dusty Explorer</h3>
+              <span className="purchase-amount">$75</span>
+              <span>Ten Hour Pass</span>
+              <button onClick={() => paymentFunc('price_1HLE74LSQ0UOTq7wxHqkfNUZ')}>Select</button>
+            </div>
+            <div className="purchase-item">
+              <h3>Founder's Package*</h3>
+              <span className="purchase-amount">$150</span>
+              <span>24 Hour Pass</span>
+              <button onClick={() => paymentFunc('price_1HLE7QLSQ0UOTq7w2uS5OOuy')}>Select</button>
+              <a 
+                href={props.user && props.user.playerData.AllowPCClient ? "https://drive.google.com/file/d/1TzWSWrMCF_q4R_0todXqMDUUM0ZIyVa3/view" : '#'}
+                className="purchase-remark"
+              ><span>*includes executable file</span></a>
+            </div>          
           </div>
-          <div className="purchase-item">
-            <h3>Weekend Warrior</h3>
-            <span className="purchase-amount">$40</span>
-            <span>Five Hour Pass</span>
-            <button onClick={() => paymentFunc('price_1HLE6iLSQ0UOTq7wVOsoU30M')}>Select</button>
-          </div>
-          <div className="purchase-item">
-            <h3>Dusty Explorer</h3>
-            <span className="purchase-amount">$75</span>
-            <span>Ten Hour Pass</span>
-            <button onClick={() => paymentFunc('price_1HLE74LSQ0UOTq7wxHqkfNUZ')}>Select</button>
-          </div>
-          <div className="purchase-item">
-            <h3>Founder's Package*</h3>
-            <span className="purchase-amount">$150</span>
-            <span>24 Hour Pass</span>
-            <button onClick={() => paymentFunc('price_1HLE7QLSQ0UOTq7w2uS5OOuy')}>Select</button>
-            <a 
-              href={props.user && props.user.playerData.AllowPCClient ? "https://drive.google.com/file/d/1TzWSWrMCF_q4R_0todXqMDUUM0ZIyVa3/view" : '#'}
-              className="purchase-remark"
-            ><span>*includes executable file</span></a>
-          </div>
+          <div className="remark">*In game time will be clocked between login and logout. As long as you logout of the game whatever time you have remaining will be valid the next time you log in.</div> <br/>
+          <div className="remark remark-second">Experience is optimized for keyboard and mouse input, touch devices experience some interactive limitations.</div>
         </div>
-        <p className="remark">*In game time will be clocked between login and logout. As long as you logout of the game whatever time you have remaining will be valid the next time you log in.</p> 
-        <p className="remark remark-second">Experience is optimized for keyboard and mouse input, touch devices experience some interactive limitations.</p>
+       )}
         <div className="workspace-block">
           <h2>Downloadable Game:</h2>
           <p className="reqs">If you have a current gaming PC (gtx 1080 or better) you can purchase our downloadable executable file for $100 for unlimited play all week long. </p>
@@ -171,6 +198,7 @@ const Workspace = (props) => {
             ><span>*includes executable file</span></a>
           </div>
         </div>
+            
         <p className="desc">We want as many people as possible to experience the magic of The Infinite Playa! We have worked diligently to make it playable in web browsers and mobile devices, for those without high end computers. However, the technology that makes this possible comes with unavoidable per minute costs. For those that find the cost of the interactive experience prohibitive, please visit https://watch.infiniteplaya.com/ where you can view most of the performances, talks and art for free.</p>
       </div>
       <Footer />
